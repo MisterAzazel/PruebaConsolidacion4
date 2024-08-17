e86fb33d2cf996ab8ca4716da6d6d0352dec8523
Josué Peñaloza <josue20650@gmail.com>
2024-08-17 00:50:07 -0400

    Añadi más variedad y cambie el precio de los productos

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,10 +32,10 @@ class Carrito {
 }
 
 function seleccionarProducto() {
-    let seleccion = prompt(`Seleccione un producto:\n1. Zelda Tears of the Kingdom - 50000\n2. Zelda1 - 50000\n3. Zelda2 - 50000\n4. Zelda3 - 50000\n5. Zelda4 - 50000\n6. Zelda5 - 50000`);
+    let seleccion = prompt(`Seleccione un producto:\n1. Zelda Tears of the Kingdom - 50000\n2. Dragon Ball: Sparking Zero1 - 55000\n3. Kingdom Hearts 2 - 12000\n4. Darksiders 3 - 35000\n5. Monster Hunter: Rise - 40000\n6. Xenoblade Chronicles 3 - 45000`);
 
     while (isNaN(seleccion) || seleccion < 1 || seleccion > 6) {
-        seleccion = prompt(`Selección inválida. Por favor, seleccione un número entre 1 y 6: \n1. Zelda Tears of the Kingdom - 50000\n2. Zelda1 - 50000\n3. Zelda2 - 50000\n4. Zelda3 - 50000\n5. Zelda4 - 50000\n6. Zelda5 - 50000`);
+        seleccion = prompt(`Selección inválida. Por favor, seleccione un número entre 1 y 6: \n1. Zelda Tears of the Kingdom - 50000\n2. Dragon Ball: Sparking Zero - 55000\n3. Kingdom Hearts 2 - 12000\n4. Darksiders 3 - 35000\n5. Monster Hunter: Rise - 40000\n6. Xenoblade Chronicles 3 - 45000`);
     }
     return seleccion;
 }
@@ -51,12 +51,12 @@ function solicitarCantidad() {
 function agregarProductosAlCarrito(carrito) {
     let seguir = true;
     const productosDisponibles = [
-        new Producto("Zelda Tears of the Kingdom", 50000),
-        new Producto("Zelda1", 50000),
-        new Producto("Zelda2", 50000),
-        new Producto("Zelda3", 50000),
-        new Producto("Zelda4", 50000),
-        new Producto("Zelda5", 50000)
+        new Producto("Zelda Tears Of The Kingdom", 50000),
+        new Producto("Dragon Ball: Sparking Zero", 55000),
+        new Producto("Kingdom Hearts 2", 12000),
+        new Producto("Darksiders 3", 35000),
+        new Producto("Monster Hunter: Rise", 40000),
+        new Producto("Xenoblade Chronicles 3", 45000)
     ];
 
     while (seguir) {
